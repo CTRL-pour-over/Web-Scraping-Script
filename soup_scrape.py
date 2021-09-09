75ca130b740a8051987687b6e69fff3e007a01f0
Thomas <tventrone95@gmail.com>
2021-09-09 12:33:24 -0400

    Parser fix

--- soup_scrape.py
+++ soup_scrape.py
@@ -3,9 +3,6 @@ from bs4 import BeautifulSoup
 from requests.models import MissingSchema
 
 class Scraper:
-    def __init__(self):
-        self.requested_doc = ""
-        
     def request_html_page(self):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0'}
         try:
@@ -14,7 +11,7 @@ class Scraper:
         except MissingSchema:
             print("Invalid URL")    
 
-    def view_raw_html(self, requested_doc):
+    def view_raw_html(self, arg_requested_doc):
         while True:
             try:
                 x = int(input("""
@@ -28,34 +25,26 @@ class Scraper:
                             \n>> """))
 
                 if x == 1:
-                    print(f"""\n----STATUS CODE: {requested_doc.status_code}\n""")
+                    print(f"""\n----STATUS CODE: {arg_requested_doc.status_code}\n""")
                 elif x == 2:
-                    print(f"""\n----HEADERS: {requested_doc.headers}\n""")
+                    print(f"""\n----HEADERS: {arg_requested_doc.headers}\n""")
                 elif x == 3:
-                    print(f"""\n----ENCODING: {requested_doc.encoding}\n""")
+                    print(f"""\n----ENCODING: {arg_requested_doc.encoding}\n""")
                 elif x == 4:
-                    print(f"""\n----TEXT: {requested_doc.text}\n""")
+                    print(f"""\n----TEXT: {arg_requested_doc.text}\n""")
                 elif x == 5:
-                    print(f"""\n----JSON: {requested_doc.json}\n""")
+                    print(f"""\n----JSON: {arg_requested_doc.json}\n""")
                 elif x == 6:
-                    return self.parse_data() 
+                    self.parse_data(arg_requested_doc) 
 
             except ValueError:
                 print("Incorrect Data Type Used. Script requires an integer to be used.")
 
-    def parse_data(self):
-        soup = BeautifulSoup(self.requested_doc, 'html.parser')
-        print(f"""
-                {soup.title}
-                {soup.title.name}
-                {soup.title.string}
-                {soup.title.parent.name}
-                {soup.p} 
-                {soup.p['class']}
-                {soup.a}
-                {soup.find_all('a')}
-                {soup.find(id="link3")}     
-                """)
+    def parse_data(self, arg_requested_doc):
+        soup = BeautifulSoup(arg_requested_doc.text, "lxml")
+        print(soup)
+             
+              
 
 def run():
     scrape_instance = Scraper()    
