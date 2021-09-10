0512e47af875d459f8671b0134b2a564931095c5
Brett Schreiber <schreiberbrett@gmail.com>
2021-09-10 13:04:36 -0400

    Refactor ScrapeManager

--- main.py
+++ main.py
@@ -1,50 +1,65 @@
+from typing import Callable, Dict, Void
 import requests
 from bs4 import BeautifulSoup
-from requests.models import MissingSchema
+from requests.models import MissingSchema, Response
+from enum import Enum
 
 class ScrapeManager:
     def __init__(self):
-        requested_doc = ''
+        self.response: Response = None
 
     def request_html_page(self):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0'}
         try:
-            self.requested_doc = requests.get(input("ENTER URL:\n>> "), headers=headers)
+            self.response = requests.get(input("ENTER URL:\n>> "), headers=headers)
             self.view_raw_html()
         except MissingSchema:
             print("Invalid URL")    
 
     def view_raw_html(self):
-        while True:
-            try:
-                x = int(input("""
-                            TYPE AN INT TO SHOW DATA:
-                            1) STATUS CODE
-                            2) HEADERS
-                            3) ENCODING
-                            4) TEXT
-                            5) JSON
-                            6) PARSE DATA
-                            \n>> """))
-                if x == 1:
-                    print(f"""\n----STATUS CODE: {self.requested_doc.status_code}\n""")
-                elif x == 2:
-                    print(f"""\n----HEADERS: {self.requested_doc.headers}\n""")
-                elif x == 3:
-                    print(f"""\n----ENCODING: {self.requested_doc.encoding}\n""")
-                elif x == 4:
-                    print(f"""\n----TEXT: {self.requested_doc.text}\n""")
-                elif x == 5:
-                    print(f"""\n----JSON: {self.requested_doc.json}\n""")
-                elif x == 6:
-                    return self.parse_document() 
-            except ValueError:
-                print("Incorrect Data Type Used. Script Requires Int.")
-            
+        mapping: Dict[UserSelection, Callable[[], None]] = {
+            UserSelection.STATUS_CODE: lambda: print(f"""\n----STATUS CODE: {self.response.status_code}\n"""),
+            UserSelection.HEADERS:     lambda: print(f"""\n----HEADERS: {self.response.headers}\n"""),
+            UserSelection.ENCODING:    lambda: print(f"""\n----ENCODING: {self.response.encoding}\n"""),
+            UserSelection.TEXT:        lambda: print(f"""\n----TEXT: {self.response.text}\n"""),
+            UserSelection.JSON:        lambda: print(f"""\n----JSON: {self.response.json}\n"""),
+            UserSelection.PARSE_DATA:  lambda: self.parse_document()
+        }
+
+        mapping[get_user_selection()]
+
     def parse_document(self):
-        soup = BeautifulSoup(self.requested_doc.text, "lxml") 
+        soup = BeautifulSoup(self.response.text, "lxml") 
         print(soup.prettify())
  
+
+class UserSelection(Enum):
+    STATUS_CODE = 1
+    HEADERS = 2
+    ENCODING = 3
+    TEXT = 4
+    JSON = 5
+    PARSE_DATA = 6
+
+def get_user_selection() -> UserSelection:
+    while True:
+        try:
+            x = int(input("""
+                        TYPE AN INT TO SHOW DATA:
+                        1) STATUS CODE
+                        2) HEADERS
+                        3) ENCODING
+                        4) TEXT
+                        5) JSON
+                        6) PARSE DATA
+                        \n>> """))
+
+            if 1 <= x <= 6:
+                return UserSelection(x)
+
+        except ValueError:
+            print("Incorrect Data Type Used. Script Requires Int.")
+
 def run():
     scrape_instance = ScrapeManager()    
     scrape_instance.request_html_page()
