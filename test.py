7099ad57f8156752bb93c24a57b7f044b8cf2a24
RIP Internet Anonymity <PuReDBLRainbow@gmail.com>
2021-09-09 21:06:05 -0400

    fixed the math error on line 96. turns out i just didn't know how to use regex and stripped out the decimal

--- test.py
+++ test.py
@@ -100,10 +100,10 @@ for contents in col:
             type = re.search('>(.+?)</td>', str(typeelem)).group(1)
             price2 = price.strip("$") #strips out $ symbol so price can be used in calculations
             thcpercent = thc.strip("%") # strips out % symbol so thc percentage can be used in calculations
-            convwt = re.sub("[^0-9]", "", wt)
+            convwt = re.sub("[^0-9.]", "", wt) # strips everything out of weight except for numbers and periods
             match = re.match('\d+mg', str(wt), re.IGNORECASE)#checks to see if it's in milligrams
             if match is None: #runs this if it's not in milligrams to convert to milligrams (making the assumption it's in grams instead)
-                convwt = float(convwt)*100 # why is this like this? 1 * 100 is not 1000
+                convwt = float(convwt)*1000
             thcwt = (float(convwt) * (float(thcpercent)/100)) #the amount of substance that's actual thc, achieved by multiplying the total weight/mass by the percentage of thc/100 SOME OF THE SHIT IS IN GRAMS LIKE 1G
             #print(strain + " " + wt + " " + thc + " " + str(price2) + " " + str(thcwt) + " " + str(thcpercent) + " " + str(convwt))
             ppthc = (float(price2)/float(thcwt)) #the price of 1mg of thc per substance. like golf, lower = better
